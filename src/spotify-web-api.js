06c02352ed4da1e1e907c6a2ce334e33f8e22e72
Ruurd Bijlsma <ruurd@bijlsma.dev>
2024-09-30 21:53:04 +0200

    Fix call to removeFromMySavedTracks

--- src/spotify-web-api.js
+++ src/spotify-web-api.js
@@ -270,7 +270,9 @@ var SpotifyWebApi = (function () {
     var requestData = {
       url: _baseUri + '/me/tracks',
       type: 'DELETE',
-      postData: trackIds
+      postData: {
+        ids: trackIds
+      }
     };
     return _checkParamsAndPerformRequest(requestData, options, callback);
   };
